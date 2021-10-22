a860974362248e16723fdd0ec8182a899924d9de
Hari Bheesetti <hbheesetti@ntwist.com>
2021-10-22 11:35:50 -0600

    adding basic attributes to Habit, instance and User classes

---
+++ AgileSprinters/app/src/main/java/com/example/agilesprinters/Habit.java
@@ -0,0 +1,10 @@
+package com.example.agilesprinters;
+
+import java.util.Date;
+
+public class Habit {
+    private String title;
+    private String reason;
+    private Date date_to_Start;
+
+}
---
+++ AgileSprinters/app/src/main/java/com/example/agilesprinters/User.java
@@ -0,0 +1,7 @@
+package com.example.agilesprinters;
+
+public class User {
+    private String username;
+    private String password;
+    private String userData;
+}
