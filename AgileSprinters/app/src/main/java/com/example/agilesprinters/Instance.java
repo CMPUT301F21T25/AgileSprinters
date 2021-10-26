2938f02f99861ee82840af167d7f68aef8f57ab9
Riya Patel <riyaben@ualberta.ca>
2021-10-26 16:12:58 -0600

    Adding template

---
+++ AgileSprinters/app/src/main/java/com/example/agilesprinters/Instance.java
@@ -0,0 +1,11 @@
+package com.example.agilesprinters;
+
+import java.util.Date;
+
+public class Instance {
+    private boolean status;
+    private String Opt_comment;
+    private Date date;
+    private int duration;
+
+}
