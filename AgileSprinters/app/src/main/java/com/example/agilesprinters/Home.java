ce098b27bc1dbba0fc92d28ceba1c8e8db101cd0
Hrose3056 <rose3056@hotmail.com>
2021-10-30 16:06:31 -0600

    Completed delete dialog fragment and added Javadoc comments to my classes

--- AgileSprinters/app/src/main/java/com/example/agilesprinters/Home.java
+++ AgileSprinters/app/src/main/java/com/example/agilesprinters/Home.java
@@ -14,7 +14,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 
 public class Home extends AppCompatActivity implements addHabitFragment.OnFragmentInteractionListener,
-        viewEditHabitFragment.OnFragmentInteractionListener{
+        viewEditHabitFragment.OnFragmentInteractionListener, deleteHabitFragment.OnFragmentInteractionListener{
     ArrayList<Habit> habitArrayList;
     ListView habitList;
     ArrayAdapter<Habit> habitAdapter;
@@ -29,14 +29,12 @@ public class Home extends AppCompatActivity implements addHabitFragment.OnFragme
 
         habitAdapter = new habitListAdapter(this, R.layout.home_list_content, habitArrayList);
         habitList.setAdapter(habitAdapter);
-        ArrayList<String> list = new ArrayList<>();
 
         habitList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Habit habit = (Habit) adapterView.getItemAtPosition(i);
-                viewEditHabitFragment values =
-                        new viewEditHabitFragment().newInstance(i, habit);
+                viewEditHabitFragment values = new viewEditHabitFragment().newInstance(i, habit);
                 values.show(getSupportFragmentManager(), "VIEW/EDIT");
             }
         });
@@ -49,8 +47,24 @@ public class Home extends AppCompatActivity implements addHabitFragment.OnFragme
             }
         });
 
+        habitList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                deleteHabitFragment delete = new deleteHabitFragment().newInstance(i);
+                delete.show(getSupportFragmentManager(), "DELETE");
+
+                // return true so that it overrides a regular item click and the view/edit fragment does not pop up
+                return true;
+            }
+        });
+
     }
 
+    /**
+     * This function adds a habit to the list once the user clicks add on the addHabitFragment
+     * dialog fragment
+     * @param habit The habit object created by the addHabitFragment
+     */
     @Override
     public void onAddPressed(Habit habit) {
         habitAdapter.add(habit);
@@ -58,6 +72,13 @@ public class Home extends AppCompatActivity implements addHabitFragment.OnFragme
         habitAdapter.notifyDataSetChanged();
     }
 
+    /**
+     * This function edits a habit in the list by taking the habit created when the "Save Changes"
+     * button is pressed and replacing the old habit object in the list with the one supplied
+     * to the function.
+     * @param habit The habit object created by the editViewHabitFragment
+     * @param position The position of the original habit object in the list
+     */
     @Override
     public void onEditViewSaveChangesPressed(Habit habit, int position) {
         Habit original = habitAdapter.getItem(position);
@@ -77,4 +98,15 @@ public class Home extends AppCompatActivity implements addHabitFragment.OnFragme
         habitAdapter.notifyDataSetChanged();
     }
 
+    /**
+     * This function deletes the object selected by the user in the list after a user clicks "Yes"
+     * in the deleteHabitFragment dialog fragment.
+     * @param position The position of the object clicked in the list.
+     */
+    @Override
+    public void onDeleteHabitYesPressed(int position) {
+        Habit habit = habitAdapter.getItem(position);
+        habitAdapter.remove(habit);
+        habitAdapter.notifyDataSetChanged();
+    }
 }
