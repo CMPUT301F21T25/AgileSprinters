8560d711e5e36d4095c76b89006bea8a5ebef157
Hrose3056 <rose3056@hotmail.com>
2021-11-05 11:22:34 -0600

    Added Javadoc for home habit list adapter

--- AgileSprinters/app/src/main/java/com/example/agilesprinters/habitListAdapter.java
+++ AgileSprinters/app/src/main/java/com/example/agilesprinters/habitListAdapter.java
@@ -22,12 +22,25 @@ public class habitListAdapter extends ArrayAdapter<Habit> {
     private Context mContext;
     private ArrayList<Habit> habits;
 
+    /**
+     * This function initializes the array list of habits and the context of the screen.
+     * @param context The current screen.
+     * @param objects The arraylist of object we are using our custom adapter on.
+     */
     public habitListAdapter(Context context,  ArrayList<Habit> objects) {
         super(context,0, objects);
         this.habits = objects;
         this.mContext = context;
     }
 
+    /**
+     * This method converts the view for the list into a custom view.
+     * @param position Position of the habit.
+     * @param convertView The converted view.
+     * @param parent The parent of the view we are changing.
+     * @return
+     * Returns the view after it's been converted and it's values set.
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
